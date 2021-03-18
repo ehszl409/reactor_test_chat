7f93f8723772a58ec8ab36d337e5bbf717a46d27
PARKS <cles2459@hanmail.net>
2021-03-18 17:22:37 +0900

    1. 채팅 구현 v1 03.18

---
+++ src/main/java/com/park/webfluxex/Content.java
@@ -0,0 +1,9 @@
+package com.park.webfluxex;
+
+import lombok.Data;
+
+@Data
+public class Content {
+	private String username;
+	private String talk;
+}
---
+++ src/main/java/com/park/webfluxex/ReactorTestChatApplication.java
@@ -0,0 +1,13 @@
+package com.park.webfluxex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorTestChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorTestChatApplication.class, args);
+	}
+
+}
